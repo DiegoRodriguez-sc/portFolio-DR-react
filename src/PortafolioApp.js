1fe3494934d41ba59f2731b3a4b05e625901ecf3
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-09 22:17:22 -0300

    project init and menu added

---
+++ src/PortafolioApp.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const Portafolioapp = () => {
+
+  const [menu, setMenu] = useState(false);
+
+ return (
+  <div>
+   {/* TODO: menu 
+            nav
+            header
+            about
+            projects
+            contact
+            footer
+    */}
+  </div>
+ );
+}
+
+export default Portafolioapp;
