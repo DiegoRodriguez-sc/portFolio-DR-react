dc62b0c5d50a2698b1b7e61362613a9abc267c5f
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-12-21 22:48:17 -0300

    core ignorecase change to false

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Project from "./Project";
+// styles
+import "./projects.css";
+import { data } from "../../helpers/data";
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <div className="projects-container">
+        <div className="heading">
+          <h3 className="title">My Works</h3>
+          <p className="separator" />
+          <p className="subtitle">
+            Here's a list of most of the projects I've been working on lately.
+          </p>
+        </div>
+        <div className="projects-wrapper">
+          {data.map((proyect) => (
+            <Project
+              key={proyect.title}
+              title={proyect.title}
+              img={proyect.img}
+              tech={proyect.tech}
+              link={proyect.link}
+              repo={proyect.repo}
+            >
+              <small>{proyect.small}</small>
+              <p>{proyect.p}</p>
+            </Project>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
